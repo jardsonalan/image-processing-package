ddff6b9b058f52de50db04036f81e7fe67f17c85
Jardson Alan <jardsonalan132@gmail.com>
2025-03-14 18:00:38 -0300

    feat: arquivos de configuração para publicação do pacote

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description='Image Processing Package using Skimage',
     long_description=page_description,
     long_description_content_type='text/markdown',
-    url='',
+    url='https://github.com/jardsonalan/image-processing-package',
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.13',
